254415e47f81c880c9bb45a0ed1ef00bc1a5f557
PaksF <flaksfjtm@gmail.com>
2024-04-11 11:20:10 +0900

    Tymeleaf 속성 inLine, Block, Link, 숫자 & 날짜 형식지정

--- src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
+++ src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
@@ -1,17 +1,20 @@
 package kr.ac.kopo.webapplication.controller;
 
 import kr.ac.kopo.webapplication.dto.SampleDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@Slf4j
 @Controller
 @RequestMapping("/sample")
 public class SampleController {
@@ -21,7 +24,7 @@ public class SampleController {
 
     }
 
-    @GetMapping("/ex2")
+    @GetMapping({"/ex2", "/exBlock", "/exLink"})
     public void exModel(Model model){
         List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i -> {
             SampleDTO dto = SampleDTO.builder()
@@ -35,4 +38,25 @@ public class SampleController {
 
         model.addAttribute("list", list);
     }
+
+    @GetMapping("/exInline")
+    public String exInline(RedirectAttributes redirectAttributes){
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("first...100")
+                .last("last...100")
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result", "Success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3");
+    }
+
 }
