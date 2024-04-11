edbd8934f52b6e157f9b203210b5e6dc9df51720
PaksF <flaksfjtm@gmail.com>
2024-04-11 11:52:03 +0900

    Tymeleaf의 Layout

--- src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
+++ src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
@@ -54,7 +54,7 @@ public class SampleController {
         return "redirect:/sample/ex3";
     }
 
-    @GetMapping("/ex3")
+    @GetMapping({"/ex3", "/exLayout1"})
     public void ex3(){
         log.info("ex3");
     }
